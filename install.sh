ffc095db8d5ec9c968b5447f1db4c329402918d5
kLeZ <julius8774@gmail.com>
2014-02-14 16:50:13 +0100

    Implemented naming conventions from kde-plasma-applets project

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 # Copyright © Alessandro Accardo 2014. All rights reserved.
 #
 
-plasmapkg -r vacation-calculator.plasmoid
-rm vacation-calculator.plasmoid
+plasmapkg -r plasma-applet-vacation-calculator.plasmoid
+rm plasma-applet-vacation-calculator.plasmoid
 zip -r vacation-calculator.zip ./{README.md,LICENSE,metadata.desktop,contents}
-mv vacation-calculator.zip vacation-calculator.plasmoid
-plasmapkg -i vacation-calculator.plasmoid
+mv vacation-calculator.zip plasma-applet-vacation-calculator.plasmoid
+plasmapkg -i plasma-applet-vacation-calculator.plasmoid
 plasmoidviewer vacation-calculator
